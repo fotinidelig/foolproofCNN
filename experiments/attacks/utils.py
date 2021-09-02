4cbfd511a983609a921531fc15bb1c280d0f175d
Fotini Delig <fotinidelig@gmail.com>
2021-09-02 16:54:00 +0300

    binary search for CONST

--- experiments/attacks/utils.py
+++ experiments/attacks/utils.py
@@ -7,19 +7,42 @@ class BaseAttack():
     def __init__(
         self,
         const: int,
-        conf: Optional[int] = 0,
-        iterations: Optional[int] = 10000
+        conf: int,
+        iterations: int,
+        max_const: float,
+        min_const: float
     ):
         self.const = const
         self.conf = conf
         self.iterations = iterations
         self.advset = []
+        self.max_const = max_const
+        self.min_const = min_const
 
     def loss(self, w, x, **kwargs):
         raise NotImplementedError
+
+    def bin_search_const(const, fx, best_fx):
+        """
+            Binary search for const in range
+            [min_const, max_const].
+
+            Return smallest const for which f(x) moves to 0
+            or end search if const is found.
+        """
+        end_iters = False
+        if fx > 0:
+            min_const = const
+            const = .5(self.max_const+self.min_const)
+        if fx <= 0:
+            max_const = const
+            const = .5(self.max_const+self.min_const)
+        if fx*best_fx <= 0: # const found in previous iteration
+            end_iters = True
+        return const, end_iters
 
     def attack(self, samples, targets):
         raise NotImplementedError
 
     def test_attack():
-        raise NotImplementedError  
+        raise NotImplementedError
