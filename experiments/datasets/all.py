c7becd60da622659e4482ebfb22926c935a97dd9
Fotini Deligiannaki <44481663+fotinidelig@users.noreply.github.com>
2021-09-13 18:46:24 +0300

    Update all.py

--- experiments/datasets/all.py
+++ experiments/datasets/all.py
@@ -7,14 +7,14 @@ import torchvision.transforms as transforms
 
 def load_cifar10(batch_size = 128, num_workers = 1, root = './data'):
     trainset = CIFAR10(root=root, train=True,
-                                    download=True, transform=transforms.ToTensor())
+                       download=True, transform=transforms.ToTensor())
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                              shuffle=True, num_workers=num_workers, pin_memory=True)
+                       shuffle=True, num_workers=num_workers, pin_memory=True)
 
     testset = CIFAR10(root=root, train=False,
-                                           download=True, transform=transforms.ToTensor())
+                       download=True, transform=transforms.ToTensor())
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                             shuffle=False, num_workers=num_workers, pin_memory=True)
+                       shuffle=False, num_workers=num_workers, pin_memory=True)
 
     print("\n Dataset: %s \n Trainset: %d samples\n Testset: %d samples\n BATCH_SIZE: %d \n Classes: %d \n"%
           ("Cifar10",trainset.__len__(),testset.__len__(), batch_size, len(trainset.classes)))
@@ -23,14 +23,14 @@ def load_cifar10(batch_size = 128, num_workers = 1, root = './data'):
 
 def load_mnist(batch_size = 128, num_workers = 1, root = './data'):
     trainset = MNIST(root=root, train=True,
-                                    download=True, transform=transforms.ToTensor())
+                      download=True, transform=transforms.ToTensor())
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                              shuffle=True, num_workers=num_workers, pin_memory=True)
+                      shuffle=True, num_workers=num_workers, pin_memory=True)
 
     testset = MNIST(root=root, train=False,
-                                           download=True, transform=transforms.ToTensor())
+                       download=True, transform=transforms.ToTensor())
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                             shuffle=False, num_workers=num_workers, pin_memory=True)
+                       shuffle=False, num_workers=num_workers, pin_memory=True)
 
     print("\n Dataset: %s \n Trainset: %d samples\n Testset: %d samples\n BATCH_SIZE: %d \n Classes: %d \n"%
           ("MNIST",trainset.__len__(),testset.__len__(), batch_size, len(trainset.classes)))
