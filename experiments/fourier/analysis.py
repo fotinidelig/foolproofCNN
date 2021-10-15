f574bc5b7d4f6e9002f724da2e645edf2a1322f5
Fotini Delig <fotinidelig@gmail.com>
2021-10-16 00:05:43 +0300

    fourier transform obj

--- experiments/fourier/analysis.py
+++ experiments/fourier/analysis.py
@@ -13,9 +13,17 @@ from torch.fft import fft2, ifft2, fftshift, ifftshift
 
 plt.rcParams["font.family"] = "serif"
 
+def CHW_to_HWC(image, inverse = False):
+    if not inverse and torch.argmin(torch.tensor(image.size())) == 0:
+        image = image.transpose(1,0).transpose(1,2)
+    if inverse and torch.argmin(torch.tensor(image.size())) == 2:
+        image = image.transpose(1,2).transpose(1,0)
+    return image
+
 def toDFT(
-    images: torch.tensor # shape (N, C, W, H) or (C, W, H)
+    images: torch.tensor # shape (N, C, H, W) or (C, H, W)
 ):
+    images = CHW_to_HWC(images, inverse = True)
     transformed = fftshift(fft2(images), dim=(1,2)) # move origin to center
     amps = transformed.abs()
     phase = transformed.angle() # in rads
@@ -25,13 +33,16 @@ def fromDFT(
     amps: torch.tensor,
     phase: torch.tensor,
 ):
+    amps = CHW_to_HWC(amps, inverse = True)
+    phase = CHW_to_HWC(phase, inverse = True)
     complex = amps*(np.cos(phase)+1j*np.sin(phase))
     inversed = ifft2(ifftshift(complex, dim=(1,2)))
-    return inversed.abs().int()
+    return inversed.abs()
 
 ## Filtering - High, Low, Band Pass
 ## All filters expect images of shape (C, H, W)
 def xLP(amps, threshold):
+    amps = CHW_to_HWC(amps, inverse=True)
     H = amps.shape[1]
     W = amps.shape[2]
 
@@ -43,12 +54,13 @@ def xLP(amps, threshold):
     return amps-filtered
 
 def xHP(amps, threshold):
-    H = amps.shape[0]
-    W = amps.shape[1]
+    amps = CHW_to_HWC(amps, inverse=True)
+    H = amps.shape[1]
+    W = amps.shape[2]
 
     center = (int(H/2), int(W/2))
     filtered = amps.clone()
-
+    # filter out low frequencies in range threshold -1
     x = (center[0]-threshold+1, center[0]+threshold)
     y = (center[1]-threshold+1, center[1]+threshold)
     filtered[:,x[0]:x[1], y[0]:y[1]] = 0
@@ -67,10 +79,13 @@ def filterImage(images: torch.tensor, filter, threshold = Union[int, tuple]):
     '''
     if filter == xBP and not isinstance(threshold, tuple):
         raise("RuntimeError: if filter = xBP, threshold must be tuple of size 2")
-
+    orig_shape = images.shape
     _, amps, phases = toDFT(images)
     threshold = threshold if isinstance(threshold, tuple) else (threshold,)
     amps = filter(amps, *threshold)
+    if (amps.shape != orig_shape):
+        phases = CHW_to_HWC(phases, inverse=True)
+
     images = fromDFT(amps, phases)
     images = (images-images.min())/(images.max()-images.min())
     return images
